e4d8ddc6137833ac4bd86e06cbc5f0430a29f14b
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-05-09 21:27:48 -0300

    Primeiros Ponteiros em C

---
+++ 06-PONTEIROS E ALOCAÇÃO DINAMICA/4-CUSTOS-DE-TRANSPORTE/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// 3 bibliotecas que vc sempre vai importar mesmo que não use.
+
+/*
+  Os elementos aij de uma matriz inteira A
+  nxn representam os custos de transporte da 
+  cidade i para a cidade j. Dados n itinerários, cada um com k cidades, calcular o 
+  custo total para cada itinerário. Exemplo:
+  O custo do itinerário 0 3 1 3 3 2 1 0 é a03 + a31 + a13 + a33 + a32 + a21 + a10 = 3 + 1 + 
+  400 + 5 + 2 + 1 + 5 = 417
+*/
+
+// não entendi a questão
+
+
+int main(void) {
+
+
+  // ENTRADA
+  
+
+  // PROCESSAMENTO
+
+  // SAÍDA
+
+
+  
+  
+  
+  return 0;
+}
