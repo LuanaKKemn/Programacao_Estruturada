b8be533c702a92806333b74cb8287c942faa7922
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-05-11 23:17:12 -0300

    Primeiras Strings Em C

---
+++ 07-STRINGS/lab-str-cmp-recursiva/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void cmpRecursiva();
+
+int main(void) {
+  char a[30];
+  char b[30];
+
+  // ENTRADA
+  printf("Digite a 1ª string: ");
+  scanf("%s", &a[30]);
+
+  printf("Digite a 2ª string: ");
+  scanf("%s", &b[30]);
+
+
+  // PROCESSAMENTO
+
+
+
+  // SAÍDA
+
+  
+  return 0;
+}
+
+void cmpRecursiva(char a, char b, int i){
+  if (strlen(a) != strlen(char b))
+  
+}
