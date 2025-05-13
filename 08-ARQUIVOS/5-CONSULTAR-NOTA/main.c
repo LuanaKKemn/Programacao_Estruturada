62f7e19de93e5e8a6e7a5490402bc8bc97b533ca
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-05-12 22:15:29 -0300

    Primeiros Arquivos em C

---
+++ 08-ARQUIVOS/5-CONSULTAR-NOTA/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/*
+  Faça um programa que recebe o nome de um arquivo e conta o total de letras, vogais,
+  consoantes, espaços em branco, palavras e o total de linhas encontradas no arquivo.
+  Considere que uma palavra é separada de outra por um espaço em branco ou um fim de linha.
+*/
+
+int main(void) {
+
+  
+  
+  
+  return 0;
+}
